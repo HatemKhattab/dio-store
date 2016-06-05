6b72bac8c4c0890abc60e31cfbee613c26cabe5a
Hatem Khattab <you@example.com>
2016-06-06 00:49:24 +0200

    Edit product logic

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,29 +1,41 @@
 class CartController < ApplicationController
 
   def add
-  	id = params[:id]
-	  	if session[:cart] then 
-	  		cart = session[:cart]
-	    else
-	    	session[:cart] = {}
-	    	cart = session[:cart]
-	    end
-
-	    if cart[:id] then
-	    	cart[:id] = cart[:id] + 1
-	    else
-	    	cart[:id] = 1
-	    end
-	    
-	  rediect_to :action => :index
+    id = params[:id]
+ 	  cart[id] ? cart[id] = cart[id]+1 : cart[id]=1
+	  redirect_to :action => :index
   end
 
   def clearCart
-  	session[:cart] = nil
-  	redirect_to :action => :index
+    session[:cart] = nil
+    redirect_to :action => :index
   end
 
   def index
+  	@cart = cart
   end
-  
+
+private
+
+   def cart  
+     session[:cart] ||= {}
+  end
+
+ 
 end
+
+
+   #  	id = params[:id]
+	  # 	if session[:cart] then 
+	  # 		cart = session[:car]
+	  #   else
+	  #   	session[:cart] = {}
+	  #   	cart = session[:cart]
+	  #   end
+
+	  #   if cart[:id] then
+	  #   	cart[:id] = cart[:id] + 1
+	  #   else
+	  #   	cart[:id] = 1
+	  #   end
+	    
