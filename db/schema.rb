af9f739c553b33ecebedf37abdd88016eb28ce81
Hatem Khattab <you@example.com>
2016-08-08 22:36:40 +0200

    change price to decimal

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160806205312) do
+ActiveRecord::Schema.define(version: 20160808131111) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.integer  "price"
+    t.decimal  "price",       precision: 8, scale: 2
     t.string   "category"
     t.string   "subcategory"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
   end
 
   create_table "users", force: :cascade do |t|
