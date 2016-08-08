657cad246cb4719f18cc385dfa5fef0aec08d798
Hatem Khattab <you@example.com>
2016-08-08 22:56:48 +0200

    remove category and subcategory from product

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160808131111) do
+ActiveRecord::Schema.define(version: 20160808204009) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.decimal  "price",       precision: 8, scale: 2
-    t.string   "category"
-    t.string   "subcategory"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
   end
