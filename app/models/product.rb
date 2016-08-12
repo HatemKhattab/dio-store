b0464e5e1bc8d959f5abd743c978f9bd86168a62
Hatem Khattab <you@example.com>
2016-08-12 08:46:15 +0200

    delete image_if from products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ActiveRecord::Base
+	has_many :images, dependent: :destroy
+	
 	validates :title, presence: true
 	validates :price, presence: true, numericality: {:greater_than => 0, :less_than => 10000}
-	
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160809205654) do
+ActiveRecord::Schema.define(version: 20160812064446) do
 
   create_table "images", force: :cascade do |t|
     t.datetime "created_at", null: false
