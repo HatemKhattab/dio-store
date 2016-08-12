40c9f3bfd736a9b062614d4583bf9142d4caa1a3
Hatem Khattab <you@example.com>
2016-08-12 09:02:39 +0200

    Add product to image

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160812064446) do
+ActiveRecord::Schema.define(version: 20160812065742) do
 
   create_table "images", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "product_id"
   end
+
+  add_index "images", ["product_id"], name: "index_images_on_product_id"
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
