26338dd7c5e705b6a93161688b37eba6282308dd
Hatem Khattab <you@example.com>
2016-08-27 16:03:28 +0200

    edit product form

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,7 +3,7 @@ class Image < ActiveRecord::Base
 
 	has_attached_file :picture, styles: {thumb: "100x100"},
 	  url: "/images/:style/:filename",
-	  path: ":rails_root/public/images/:style/:filename"
+	  path: ":rails_root/public/images/:id/:style/:filename"
 
 	validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,6 @@ class Product < ActiveRecord::Base
 	
 	validates :title, presence: true
 	validates :price, presence: true, numericality: {:greater_than => 0, :less_than => 10000}
+    validates :images, :length => {:maximum => 9}
 end
+
