2520db21eb6f33ca3a6d67d2ef7b8b1f3a7d6d44
Hatem Khattab <you@example.com>
2016-09-01 22:01:20 +0200

    add valida less than 5 mega

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,8 +2,10 @@ class Image < ActiveRecord::Base
 	belongs_to :product
 
 	has_attached_file :picture, styles: {thumb: "100x100"},
-	  url: "/images/:style/:filename",
+	  url: "/images/:id/:style/:filename",
 	  path: ":rails_root/public/images/:id/:style/:filename"
-
-	validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/
+  
+  validates :picture,
+     attachment_content_type: { content_type: /\Aimage\/.*\Z/ },
+     attachment_size: { less_than: 5.megabytes }
 end
