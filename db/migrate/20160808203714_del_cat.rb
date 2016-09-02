14ab8faa1ed7ddfb6dc60fe7f6d3ef684f898cd4
Hatem Khattab <you@example.com>
2016-09-02 11:45:26 +0200

    remove del category migration

--- db/migrate/20160808203714_del_cat.rb
+++
@@ -1,5 +0,0 @@
-class DelCat < ActiveRecord::Migration
-  def change
-  	remove_column :products, :category, :subcategory
-  end
-end
--- db/migrate/20160808204009_del_category.rb
+++
@@ -1,6 +0,0 @@
-class DelCategory < ActiveRecord::Migration
-  def change
-  	remove_column :products, :category 
-  	remove_column :products, :subcategory
-  end
-end
