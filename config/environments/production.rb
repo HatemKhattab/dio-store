325c64138447ca6fa655b7c09801f17064358f6e
Hatem Khattab <you@example.com>
2016-09-02 10:40:19 +0200

    modify production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
     s3_credentials: {
       bucket: ENV['S3_BUCKET_NAME'], #ENV.fetch('S3_BUCKET_NAME'),
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],  #ENV.fetch('AWS_ACCESS_KEY_ID'),
-      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'] #ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],  #ENV.fetch('AWS_SECRET_ACCESS_KEY'),
       s3_region: ENV['AWS_REGION'] #ENV.fetch('AWS_REGION'),
      }
    }
