7c2723d8282e3c0eeb9803f1b0a192201ad539ef
Hatem Khattab <you@example.com>
2016-09-03 23:04:57 +0200

    edit image model

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,9 +1,9 @@
 class Image < ActiveRecord::Base
 	belongs_to :product
 
-	has_attached_file :picture, styles: {thumb: "100x100"},
-	  url: "/images/:id/:style/:filename",
-	  path: ":rails_root/public/images/:id/:style/:filename"
+	has_attached_file :picture, styles: {thumb: "100x100"}
+	  #url: "/images/:id/:style/:filename",
+	  #path: ":rails_root/public/images/:id/:style/:filename"
   
   validates :picture,
      attachment_content_type: { content_type: /\Aimage\/.*\Z/ },
