cd2c11c2e0e2b740e546cd6075f163a95fbeb897
Hatem Khattab <you@example.com>
2016-09-04 13:47:54 +0200

    edit paperclip option

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,7 +1,4 @@
-Paperclip::Attachment.default_options[:storage] = :s3
-Paperclip::Attachment.default_options[:bucket] = ENV['S3_BUCKET_NAME']
-Paperclip::Attachment.default_options[:s3_permissions] = :public_read
-Paperclip::Attachment.default_options[:s3_credentials] = {
-  access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-  secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
-}
+
+#Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+#Paperclip::Attachment.default_options[:path] = '/diostore/app/public/images/:id/:style/:filename'
+Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
