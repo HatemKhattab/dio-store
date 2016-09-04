568a826d5d47a2fe0bd0c94cd1e31080aed64c21
Hatem Khattab <you@example.com>
2016-09-04 13:52:50 +0200

    fix viewing image name on production

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,4 +1,1 @@
-
-#Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
-#Paperclip::Attachment.default_options[:path] = '/diostore/app/public/images/:id/:style/:filename'
 Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
