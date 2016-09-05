3f131f9c3a8ac49daaf2355e1d3b2d8b4f326082
Hatem Khattab <you@example.com>
2016-09-05 11:57:17 +0200

    edit products view style

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,11 +1,12 @@
 class Image < ActiveRecord::Base
 	belongs_to :product
 
-	has_attached_file :picture, styles: {thumb: "100x100"},
+	has_attached_file :picture, styles: {thumb: "100x100", medium: "300x300"},
 	  url: "/images/:id/:style/:filename",
-	  path: ":rails_root/public/images/:id/:style/:filename"
+	  path: ":rails_root/public/images/:id/:style/:filename",
+	  default_url: "http://www.replacements.com/images/no_image.jpg"
   
   validates :picture,
-     attachment_content_type: { content_type: /\Aimage\/.*\Z/ },
-     attachment_size: { less_than: 5.megabytes }
+    attachment_content_type: { content_type: /\Aimage\/.*\Z/ },
+    attachment_size: { less_than: 5.megabytes }
 end
