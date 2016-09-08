deacfe70554282ed11a4060f4af39f2e9b19a876
Hatem Khattab <you@example.com>
2016-09-08 10:26:48 +0200

    Add category to product table

---
+++ db/migrate/20160908082336_add_cat_to_products.rb
@@ -0,0 +1,7 @@
+class AddCatToProducts < ActiveRecord::Migration
+  def change
+  	change_table :products do |t|
+  		t.belongs_to :category, index: true
+  	end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160907144507) do
+ActiveRecord::Schema.define(version: 20160908082336) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -38,7 +38,10 @@ ActiveRecord::Schema.define(version: 20160907144507) do
     t.decimal  "price",       precision: 8, scale: 2
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.integer  "category_id"
   end
+
+  add_index "products", ["category_id"], name: "index_products_on_category_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
