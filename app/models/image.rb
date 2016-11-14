ef55787693588796c4befc7837ce29821d7b0d30
Hatem Khattab <hattemkhattab@gmail.com>
2016-11-14 10:24:26 +0100

    edit default image

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,11 +1,11 @@
 class Image < ActiveRecord::Base
 	belongs_to :product 
 
-	has_attached_file :picture, styles: {thumb: "100x100", medium: "300x300"},
+	has_attached_file :picture, styles: {thumb: "100x100", medium: "300x300"}, 
 	  url: "/images/:id/:style/:filename",
 	  path: ":rails_root/public/images/:id/:style/:filename",
-	  default_url: "http://www.replacements.com/images/no_image.jpg"
-  
+	  default_url: "http://www.wonkville.net/wp-content/uploads/2016/04/No-image-available.jpg"
+
   validates :picture,
     attachment_content_type: { content_type: /\Aimage\/.*\Z/ },
     attachment_size: { less_than: 5.megabytes }
